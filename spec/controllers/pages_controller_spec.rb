e57949afd3177f8051b76450e0c10b91292a83be
Ed Rayne <edrayne@me.com>
2010-06-28 16:22:35 +0100

    Finished layout and routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -42,4 +42,16 @@ describe PagesController do
       response.should have_tag("title", @base_title + " | About")
     end
   end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_tag("title", @base_title + " | Help")
+    end
+  end
 end
